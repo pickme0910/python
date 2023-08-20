f214a388d59c58b8faec56c7bf29930a632012c2
pickme0910 <122138139+pickme0910@users.noreply.github.com>
2023-08-20 13:46:34 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# 181942
+# def solution(str1, str2):
+#     answer = ''
+#     for i in range(len(str1)):
+#         answer += str1[i]
+#         answer += str2[i]
+#     return answer
+# print(solution())
+
+# 181899
+
+# def solution(start, end):
+#     answer = []
+#     while True:
+#         if start == end:
+#             answer.append(end)
+#             break
+#         answer.append(start)
+#         start -= 1
+#     return answer
+# print(solution())
